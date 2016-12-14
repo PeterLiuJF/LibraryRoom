c4529dde1f823b9fe0ea6fe6ffe3cec579f53212
刘江丰 <jf4216990@163.com>
2016-12-14 15:13:45 +0800

    Create Project

---
+++ LibraryRoom/App_Start/BundleConfig.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace LibraryRoom
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-3.1.1.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
